f1aaf901ed39d35f761ed5ad0916df6758a2993f
Sylvestre <pasytchangwa@gmail.com>
2021-12-10 12:54:19 +0200

    Add validations for the user, reservation and house models

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,3 +1,14 @@
 class House < ApplicationRecord
   belongs_to :user
+
+  has_and_belongs_to_many :reservations
+  
+  validates :house_description, presence: true, length: { maximum: 150 }
+
+  validates :title, :image, :location, :price, :availability, :discount, presence: true
+
+  validates :title, uniqueness: true, length: { maximum: 50 }
+
+  validate :discount, :price, numericality: { min: 0 }
+
 end

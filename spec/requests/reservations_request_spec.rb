1c22d3375f11989e8e014f06b8e09167f7664f38
Tolibjon <tolibjohn@gmail.com>
2021-12-14 13:24:29 +0500

    Add delete request test to remove the reservation

--- spec/requests/reservations_request_spec.rb
+++ spec/requests/reservations_request_spec.rb
@@ -49,4 +49,21 @@ RSpec.describe 'Reservations API', type: :request do
     end
   end
 
+  describe 'DELETE /reservations/:id' do
+    before do 
+      @user = User.create(name: 'Tester')
+      @house = House.create(title:"House_1", house_description:'First house description', location:'London', image:'https://images.unsplash.com/photo-1568605114967-8130f3a36994?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80', price:250, availability:24, discount:2.5, user:@user)
+      @reservation = Reservation.create(rent_duration:6, rent_start_date:"2021-12-12", rent_total_price:"1500", user:@user)
+      @reservation.house_ids = @house.id
+    end
+
+    it 'deletes a reservation' do
+      expect{
+        delete "/api/v1/users/#{@user.id}/reservations/#{@reservation.id}"
+      }.to change {@user.reservations.count}.from(1).to(0)
+      
+      expect(response).to have_http_status(:no_content)
+    end
+  end
+
 end
